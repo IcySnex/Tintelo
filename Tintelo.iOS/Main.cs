efa43897d36f12c235cdc08dfbdfd612640db247
IcySnex <81762750+IcySnex@users.noreply.github.com>
2026-09-12 14:22:27 +0200

    Support iOS 18

--- Tintelo.iOS/Main.cs
+++ Tintelo.iOS/Main.cs
@@ -33,7 +33,7 @@ SkeleApplication.CreateBuilder()
 	})
 	.Tabs(configure => configure
 		.LargeTitles()
-		.Tab<CalendarView>(Texts.Calendar_Title, ImageSource.Symbol($"{DateTime.Now.Day}.calendar"))
+		.Tab<CalendarView>(Texts.Calendar_Title, ImageSource.Symbol(OperatingSystem.IsIOSVersionAtLeast(26) ? $"{DateTime.Now.Day}.calendar" : "calendar"))
 		.Tab<AnalyticsView>(Texts.Analytics_Title, ImageSource.Symbol("chart.bar.xaxis"))
 		.Bubble<AddViewModel>(Texts.AddEntry_Title, ImageSource.Symbol("plus"), vm => vm.ShowCommand))
 	.UseTint(Color.Dynamic(
